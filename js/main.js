fd5dd91d7f0a66a579687f1bf83b71e67decad2c
hawk198723 <hawk198723@gmail.com>
2024-02-15 13:30:31 -0800

    update new content

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Full Stack Web Developer", "Java Developer", "U.S. Veteran"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
